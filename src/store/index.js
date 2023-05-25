b44b50d611f84889e542c22de212b9eace6cb5d0
guorenjie <2397830384@qq.com>
2023-05-25 11:21:10 +0800

    test

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    //存放状态
+  },
+  mutations: {
+    //state成员操作
+  },
+  actions: {
+    //异步操作
+  },
+  modules: {
+    //模块化状态管理
+  }
+})
